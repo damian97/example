13f823dd9283f99843e512999838b3360e912195
damian97 <sryncu@o2.pl>
2022-09-05 14:53:50 +0200

    add as empty
    sdawdasdas

---
+++ untitled/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner skane = new Scanner(System.in);
+        String slowo = skane.nextLine();
+        System.out.println(slowo);
+
+    }
+}
